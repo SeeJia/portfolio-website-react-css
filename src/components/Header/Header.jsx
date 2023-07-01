1168c4cb430f091345de48f863a9a87396f1f1be
SeeJia <seejiajia1010@gmail.com>
2023-07-01 19:37:22 +0800

    home section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
+import './Header.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="home">
+      <span>I'm</span><h1> See Jia Jia,</h1>
+      <h3>Frontend Developer.</h3>
+      <p>I’m a Information Technology(IT) student studied at University Utara Malaysia(UUM).</p>
+      
+      <button className='project'><Link to="/Projects">Project</Link></button>
+    
+    </div>
   )
 }
 
