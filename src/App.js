0bc37cad35607117c2c0e27069bcc041e9375248
SeeJia <seejiajia1010@gmail.com>
2023-07-01 18:40:15 +0800

    navbar section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Navbar from './components/Navbar/Navbar';
 import Header from './components/Header/Header';
 import About from './components/About/About';
@@ -10,12 +11,19 @@ function App() {
   return (
     <div className="App">
 
+    <div className="app">
+    <Router>
       <Navbar />
-      <Header />
-      <About />
-      <Skills />
-      <Projects />
-      <Footer /> 
+      <Routes>
+        <Route path="/" element={<Header />}/>
+        <Route path="/About" element={<About />}/>
+        <Route path="/Skills" element={<Skills />}/>
+        <Route path="/Projects" element={<Projects />}/> 
+     
+      </Routes>
+      <Footer />
+     </Router>
+    </div>
      
     </div>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 
-const Navbar = () => {
-  return (
-    
-    <div>Navbar</div>
-   
-  )
-}
+export const Navbar = () => { 
 
-export default Navbar
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <h1>My Portfolio</h1>
+      </div>
+      <button className="menu-button" onClick={toggleMenu}>
+        Menu
+      </button>
+      <ul className={`menu ${isMenuOpen ? 'open' : ''}`}>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/About">About</Link></li>
+        <li><Link to="/Skills">Skills</Link></li>
+        <li><Link to="/Projects">Projects</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
