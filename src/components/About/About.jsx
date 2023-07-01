befc541b8d59d6bb7e12b813f63117cf53e504ad
SeeJia <seejiajia1010@gmail.com>
2023-07-01 20:07:10 +0800

    About section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import './About.css';
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className="about">
+      <h1>About <span>Me</span></h1>
+      <p>Hello, My name is See Jia Jia, a person who likes to build something simple and beautiful. My interest for web when I follow a developer who develops a website, it makes me wants to start design, research and develop about the website.</p>
+    
+    </div>
   )
 }
 
